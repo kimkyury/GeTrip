c3b5b2abf23d91cc8c7ef8d371672657f18db661
LSH <hoonideal@naver.com>
2023-05-24 10:33:55 +0900

    router animation

--- enjoytrip-vue/src/App.vue
+++ enjoytrip-vue/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-    <div id="app">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" />
-        <!-- Theme stylesheet-->
-        <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet" />
-        <!-- Custom stylesheet - for your changes-->
-        <link rel="stylesheet" href="css/custom.css" />
+  <div id="app">
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,700"
+    />
+    <!-- Theme stylesheet-->
+    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet" />
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="css/custom.css" />
 
-        <nav-bar></nav-bar>
-        <nav-bar-page-move></nav-bar-page-move>
-        <router-view></router-view>
-    </div>
+    <nav-bar></nav-bar>
+    <nav-bar-page-move></nav-bar-page-move>
+    <transition name="fade">
+      <router-view /> 
+    </transition>
+  </div>
 </template>
 
 <script>
@@ -19,47 +24,56 @@ import NavBarPageMove from "@/components/NavBarPageMove.vue";
 // import HomePage from "@/components/HomePage.vue";
 
 export default {
-    name: "App",
-    components: {
-        NavBar,
-        NavBarPageMove,
-    },
-    methods: {
-        injectSvgSprite(path) {
-            var ajax = new XMLHttpRequest();
-            ajax.open("GET", path, true);
-            ajax.send();
+  name: "App",
+  components: {
+    NavBar,
+    NavBarPageMove,
+  },
+  methods: {
+    injectSvgSprite(path) {
+      var ajax = new XMLHttpRequest();
+      ajax.open("GET", path, true);
+      ajax.send();
 
-            // eslint-disable-next-line no-unused-vars
-            ajax.onload = function (e) {
-                var div = document.createElement("div");
-                div.className = "d-none";
-                div.innerHTML = ajax.responseText;
-                document.body.insertBefore(div, document.body.childNodes[0]);
-            };
-        },
+      // eslint-disable-next-line no-unused-vars
+      ajax.onload = function (e) {
+        var div = document.createElement("div");
+        div.className = "d-none";
+        div.innerHTML = ajax.responseText;
+        document.body.insertBefore(div, document.body.childNodes[0]);
+      };
     },
+  },
 
-    mounted() {
-        this.injectSvgSprite("https://bootstraptemple.com/files/icons/orion-svg-sprite.svg");
-    },
+  mounted() {
+    this.injectSvgSprite(
+      "https://bootstraptemple.com/files/icons/orion-svg-sprite.svg"
+    );
+  },
 
-    metaInfo: {
-        link: [
-            {
-                rel: "stylesheet",
-                href: "https://fonts.googleapis.com/css?family=Roboto:300,400,700",
-            },
-            {
-                rel: "stylesheet",
-                href: "https://use.fontawesome.com/releases/v5.7.1/css/all.css",
-                integrity:
-                    "sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr",
-                crossorigin: "anonymous",
-            },
-        ],
-    },
+  metaInfo: {
+    link: [
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Roboto:300,400,700",
+      },
+      {
+        rel: "stylesheet",
+        href: "https://use.fontawesome.com/releases/v5.7.1/css/all.css",
+        integrity:
+          "sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr",
+        crossorigin: "anonymous",
+      },
+    ],
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.fade-enter-active, .fade-leave-active{
+    transition: opacity .3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */{
+    opacity: 0;
+}
+</style>
--- enjoytrip-vue/src/components/HomePage.vue
+++ enjoytrip-vue/src/components/HomePage.vue
@@ -3,7 +3,7 @@
         class="text-white bg-cover bg-center primary-overlay overlay-dense"
         style="background: url('img/photogrid.jpg') repeat"
     >
-        <div class="overlay-content py-5">
+        <div class="overlay-content py-5" style="height:80vh">
             <div class="container py-4">
                 <!-- Hero slider-->
                 <div class="swiper-container homepage-slider">
@@ -30,70 +30,7 @@
                                 </div>
                             </div>
                         </div>
-                        <!-- Hero Slide-->
-                        <div class="swiper-slide h-auto mb-5">
-                            <div class="row gy-5 h-100 align-items-center">
-                                <div class="col-lg-7">
-                                    <img class="img-fluid" src="img/template-mac.png" alt="" />
-                                </div>
-                                <div class="col-lg-5">
-                                    <h1 class="text-uppercase">46 HTML pages full of features</h1>
-                                    <ul class="list-unstyled text-uppercase fw-bold mb-0">
-                                        <li class="mb-2">Sliders and carousels</li>
-                                        <li class="mb-2">4 Header variations</li>
-                                        <li class="mb-2">
-                                            Google maps, Forms, Megamenu, CSS3 Animations and much
-                                            more
-                                        </li>
-                                        <li>+ 11 extra pages showing template features</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Hero Slide-->
-                        <div class="swiper-slide h-auto mb-5">
-                            <div class="row gy-5 h-100 align-items-center">
-                                <div class="col-lg-5 text-lg-end">
-                                    <h1 class="text-uppercase">Design</h1>
-                                    <ul class="list-unstyled text-uppercase fw-bold mb-0">
-                                        <li class="mb-2">Clean and elegant design</li>
-                                        <li class="mb-2">Full width and boxed mode</li>
-                                        <li class="mb-2">
-                                            Easily readable Roboto font and awesome icons
-                                        </li>
-                                        <li>7 preprepared colour variations</li>
-                                    </ul>
-                                </div>
-                                <div class="col-md-7">
-                                    <img
-                                        class="img-fluid"
-                                        src="img/template-easy-customize.png"
-                                        alt=""
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Hero Slide-->
-                        <div class="swiper-slide h-auto mb-5">
-                            <div class="row gy-5 h-100 align-items-center">
-                                <div class="col-lg-7">
-                                    <img
-                                        class="img-fluid"
-                                        src="img/template-easy-code.png"
-                                        alt=""
-                                    />
-                                </div>
-                                <div class="col-lg-5">
-                                    <h1 class="text-uppercase">Easy to customize</h1>
-                                    <ul class="list-unstyled text-uppercase fw-bold mb-0">
-                                        <li class="mb-2">7 preprepared colour variations.</li>
-                                        <li>Easily to change fonts</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
                     </div>
-                    <div class="swiper-pagination swiper-pagination-light"></div>
                 </div>
             </div>
         </div>
