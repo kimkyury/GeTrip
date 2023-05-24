0aaf8f2137a7ddef59dafaff76129c345eb1b44f
LSH <hoonideal@naver.com>
2023-05-24 16:07:03 +0900

    login modified

--- enjoytrip-spring/src/main/java/com/mycom/trip/controller/LoginController.java
+++ enjoytrip-spring/src/main/java/com/mycom/trip/controller/LoginController.java
@@ -1,12 +1,5 @@
 package com.mycom.trip.controller;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,10 +8,10 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mycom.trip.dto.UserDto;
@@ -75,6 +68,47 @@ public class LoginController {
 		} else {
 			map.put("result", "fail");
 		}
+		return map;
+	}
+	
+	@GetMapping("/login/kakao/{userEmail}")
+	public Map<String, String> kakaoLoginSearch(@PathVariable String userEmail, HttpSession session){
+		Map<String, String> map = new HashMap<>();		
+		UserDto dto = new UserDto();
+		dto.setUserEmail(userEmail);
+		UserGetDto userGetDto = loginService.login(dto);
+		
+		if (userGetDto != null) {
+			session.setAttribute("userDto", userGetDto);
+			map.put("result", "success");
+
+			map.put("userSeq", String.valueOf(userGetDto.getUserSeq()));
+			map.put("userName", userGetDto.getUserName());
+			map.put("userEmail", userGetDto.getUserEmail());
+			map.put("userRegisterDate", userGetDto.getUserRegisterDate().toString());
+
+			map.put("userProfileImageUrl", userGetDto.getUserProfileImageUrl());
+
+			map.put("userBirthYear", String.valueOf(userGetDto.getBirthYear()));
+			map.put("userBirthMonth", String.valueOf(userGetDto.getBirthMonth()));
+			map.put("userBirthDay", String.valueOf(userGetDto.getBirthDay()));
+
+			map.put("genderCode", String.valueOf(userGetDto.getGenderCode()));
+			map.put("genderName", userGetDto.getGenderName());
+
+			map.put("userSidoCode", String.valueOf(userGetDto.getSidoCode()));
+			map.put("userSidoName", userGetDto.getSidoName());
+
+			map.put("userGugunCode", String.valueOf(userGetDto.getGugunCode()));
+			map.put("userGugunName", userGetDto.getGugunName());
+
+			map.put("userClsfCode", String.valueOf(userGetDto.getUserClsfCode()));
+			map.put("userClsfName", userGetDto.getUserClsfName());
+
+		}else {
+			map.put("result","fail");
+		}
+		
 		return map;
 	}
 }
