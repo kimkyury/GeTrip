fb0910a38d74ddb7e4bf0d5e074954a1e2a25ebf
LSH <98926324+iri95@users.noreply.github.com>
2023-05-24 23:38:41 +0900

    Delete enjoytrip-test-vue directory

--- enjoytrip-test-vue/vue.config.js
+++
@@ -1,8 +0,0 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-
-module.exports = {
-    outputDir: "C:/SSAFY/KIMKYURI/SSAFY-FINAL-Workspace/Enjoytrip/src/main/resources/static",
-};
